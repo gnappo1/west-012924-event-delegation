a06f7c75b78f052940694c347e20b6a727f9327a
Matteo Piccini <gnappo.1@live.it>
2024-09-19 09:21:45 -0700

    touch ups

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+//! Rather than doing something like this
 // document.querySelectorAll('#myList li').forEach(li => {
 //   li.addEventListener('click', () => alert(`${li.innerText} was clicked`))
 // })
 
 
 
-
+//! We can set one listener on their most immediate parent and delegate
 document.getElementById("myList").addEventListener("click", function (event) {
-//   // Check if the clicked element is an <li> element
+  // Check if the clicked element is an <li> element
   if (event.target.tagName === "LI") {
     alert(event.target.textContent);
   }
